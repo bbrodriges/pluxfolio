9e39513596eeecba9a7fc58e5729f695e827c8d3
bbrodriges <bender@rodriges.org>
2011-10-12 10:43:26 +0400

    New compile functions and structure changes

--- core/lib/templates.class.php
+++ core/lib/templates.class.php
@@ -5,30 +5,53 @@
 class Templates extends Mustashe {
 	
 	private $dictionary; //Contains array of words from lang files
+	private $renderArray; //Contains all data to be passed to Mustache
+	private $siteDB; //Contains whole DB
+	private $pagetype; //Contains page type from REQUEST_URI
 
 	/* Function to be called on templates render */
 	public function init(){
 		
-		$siteDB = Database::readDB( true );
+		/* GATHERING NECESSARY SITE DATA */
+		$siteDB = Database::readDB( true ); //Reads whole DB one time
+		$themeFolder = ROOT.'themes/'.$siteDB['site']['theme'].'/'; //Contains current theme folder path
+		$renderArray = Array();
+		$dictionary = json_decode( file_get_contents( ROOT.'core/lang/'.$siteDB['site']['language'].'.json' ) , TRUE ); //opens dictionary
+		$pagetype = $_GET['pagetype'];
 		
-		$pagetype = $_GET['pagetype']; //Contains page type from REQUEST_URI
+		/* GATHERING GENERAL SITE DATA WITH CAN BE EMBEDED TO ANY PAGE */
+		/* Data from DB */
+		$renderArray['title'] = $siteDB['site']['title']; //Site title
+		$renderArray['subtitle'] = $siteDB['site']['subtitle']; //Site subtitle
+		$renderArray['artworkscounter'] = $siteDB['site']['totalartworks']; //Total artworks count
 		
-		$themeFolder = ROOT.'themes/'.Utilities::readSiteData( 'theme' ).'/';
+		/* Data from language files */
+		$renderArray['totalartworks'] = self::getTranslation( 'totalartworks' ); //Artworks counter translation
 		
-		$renderArray = Array(); //Contains all data to be passed to Mustache
+		switch ( $pagetype ) {
+			case 'article': //Article page. Get article by given pageid
+				if( !self::complileArticle( $_GET['pageid'] ) ) { //Compilation error
+					header('Location: ./error/404');
+				}
+				die;
+				break;
+			case 'gallery': //Gallery page. Get gallery by given pageid
+				die;
+				break;
+			case 'static': //Static page. Get static by given pageid
+				die;
+				break;
+			case 'tag': //Tag filtered articles. Get all articles by given tag
+				die;
+				break;
+			case 'error': //Error page. Generate error page by given pageid, e.g. 403, 404 etc.
+				die;
+				break;
+			case 'default': //Index page. Generate articles list
+				die;
+				break;
+		}
 		
-		$languageFile = ROOT.'core/lang/'.Utilities::readSiteData( 'language' ).'.json';
-		$dictionary = json_decode( file_get_contents( $languageFile ) , TRUE ); //opens dictionary
-		
-		/* Public site parts */
-		$renderArray['title'] = $siteDB['site']['title'];
-		$renderArray['subtitle'] = $siteDB['site']['subtitle'];
-		$renderArray['artworkscounter'] = $siteDB['site']['totalartworks'];
-		
-		/* Admin site parts */
-		
-		/* Language part*/
-		$renderArray['totalartworks'] = self::getTranslation( 'totalartworks' );
 		
 		/* Renders header, body and footer of page */
 		echo Mustache::render( file_get_contents( $themeFolder.'header.tpl' ) , $renderArray );
@@ -38,10 +61,46 @@ class Templates extends Mustashe {
 	}
 	
 	/* Returns translate from dictionary */
-	public function getTranslation( $id ) {
+	private function getTranslation( $id ) {
 		return $dictionary[$id];
 	}
 	
+	/* Converts http and www into clickable links */
+	private function makeLinks($text) {
+		$text = preg_replace('%(((f|ht){1}tp://)[-a-zA-^Z0-9@:\%_\+.~#?&//=]+)%i',
+		'<a href="\\1">\\1</a>', $text);
+		$text = preg_replace('%([[:space:]()[{}])(www.[-a-zA-Z0-9@:\%_\+.~#?&//=]+)%i',
+		'\\1<a href="http://\\2">\\2</a>', $text);
+		return $text;
+	}
+	
+/* ------------- COMPILATION PART ------------- */
+	
+	/* Compiles article */
+	private function complileArticle( $articleid ) {
+		if( isset( $siteDB['articles'][$articleid] ) && $siteDB['articles'][$articleid]['visible'] == 'true' ) { //Article exists and available for reading. Proceed compiling
+			$renderArray['article_title'] = $siteDB['articles'][$articleid]['title'];
+			$renderArray['article_pretext'] = self::makeLinks( $siteDB['articles'][$articleid]['pretext'] );
+			$renderArray['article_text'] = self::makeLinks( $siteDB['articles'][$articleid]['text'] );
+			$renderArray['article_tags'] = self::makeTags( $siteDB['articles'][$articleid]['tags'] );
+			$renderArray['article_date'] = date( 'Y.m.d G:i' , $siteDB['articles'][$articleid]['date'] );
+			$renderArray['article_author'] = $siteDB['articles'][$articleid]['author'];
+			return true;
+		} else { //Article does not exists. Return false to redirect to error page
+			return false;
+		}
+	}
+	
+		/* Compiles article tags */
+		private function makeTags( $tags ) {
+			$tagString = '';
+			$tags = explode( ',' , $tags );
+			foreach( $tags as $tag ){
+				$tag = trim( $tag );
+				$tagString .= ' <a href="./tag/'.$tag.'">'.$tag.'</a>,';
+			}
+			return substr( $tagString , 0 , -1 );
+		}
 }
 
 ?>
