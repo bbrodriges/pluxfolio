cc49c61d69b1ece2706fc1451a45b0734dd51049
bbrodriges <bender@rodriges.org>
2011-10-12 16:21:16 +0400

    Uncomment mustache and templates classes

--- core/includer.php
+++ core/includer.php
@@ -5,7 +5,7 @@
 	include_once('lib/galleries.class.php');
 	include_once('lib/statics.class.php');
 	include_once('lib/articles.class.php');
-	//include_once('lib/mustache.class.php'); //must be before templates.class.php
-	//include_once('lib/templates.class.php'); //Untested class
+	include_once('lib/mustache.class.php'); //must be before templates.class.php
+	include_once('lib/templates.class.php');
 
 ?>
