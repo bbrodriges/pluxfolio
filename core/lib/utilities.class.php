05059cfed2ef4f2e5ed623069c5eeb0f1117d16b
bbrodriges <bender@rodriges.org>
2011-10-13 15:43:34 +0400

    Translit function update

--- core/lib/utilities.class.php
+++ core/lib/utilities.class.php
@@ -68,21 +68,25 @@ class Utilities extends Database {
 
   /* Convert russian to translit */
   function Translit( $string ) {
+	$string = str_replace(' - ','-',$string);
+	$string = str_replace(' ','-',$string);
 	if( preg_match('/[А-Яа-яЁё]/u', $string) ) { //if any russian chars exists
-		$string = strtr( $string , "абвгдеёзийклмнопрстуфхъыэ_" , "abvgdeeziyklmnoprstufh'iei" );
-		$string= strtr ( $string , "АБВГДЕЁЗИЙКЛМНОПРСТУФХЪЫЭ_" , "ABVGDEEZIYKLMNOPRSTUFH'IEI" );
-		$string = strtr ( $string , 
-						array(
-							"ж"=>"zh", "ц"=>"ts", "ч"=>"ch", "ш"=>"sh", 
-							"щ"=>"shch","ь"=>"", "ю"=>"yu", "я"=>"ya",
-							"Ж"=>"ZH", "Ц"=>"Ts", "Ч"=>"Ch", "Ш"=>"Sh", 
-							"Щ"=>"Shch","Ь"=>"", "Ю"=>"Yu", "Я"=>"Ya",
-							"ї"=>"i", "Ї"=>"Yi", "є"=>"ie", "Є"=>"Ye"
-							)
-				 );
-		
+		$table=array(
+			"а" => "a", "б" => "b", "в" => "v", "г" => "g", "д" => "d", "е" => "e", "ё" => "yo",
+			"ж" => "zh", "з" => "z", "и" => "i", "й" => "y", "к" => "k", "л" => "l", "м" => "m",
+			"н" => "n", "о" => "o", "п" => "p", "р" => "r", "с" => "s", "т" => "t", "у" => "u",
+			"ф" => "f", "х" => "h", "ц" => "c", "ч" => "ch", "ш" => "sch", "щ" => "sh", "ъ" => "",
+			"ы" => "y", "ь" => "", "э" => "e", "ю" => "yu", "я" => "ya",
+			"А" => "A", "Б" => "B", "В" => "V", "Г" => "G", "Д" => "D", "Е" => "E", "Ё" => "YO",
+			"Ж" => "ZH", "З" => "Z", "И" => "I", "Й" => "Y", "К" => "K", "Л" => "L", "М" => "M",
+			"Н" => "N", "О" => "O", "П" => "P", "Р" => "R", "С" => "S", "Т" => "T", "У" => "U",
+			"Ф" => "F", "Х" => "H", "Ц" => "C", "Ч" => "CH", "Ш" => "SCH", "Щ" => "SH", "Ъ" => "",
+			"Ы" => "Y", "Ь" => "", "Э" => "E", "Ю" => "YU", "Я" => "YA"
+		);
+		$string = strtr( $string, $table );
 	}
-	return $string;
+	$string = preg_replace('#[^0-9a-zA-Z-]#','',$string);
+	return ucfirst( strtolower( $string ) );
   }
 
 /* ----------- END ADDITIONAL ----------------- */
