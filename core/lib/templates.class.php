ad3d7ccccf13c05bb507e54d7d21ded8b163d6df
bbrodriges <bender@rodriges.org>
2011-10-17 16:18:31 +0400

    Minor fixes

--- core/lib/templates.class.php
+++ core/lib/templates.class.php
@@ -6,6 +6,7 @@ class Templates extends Mustache {
 	
 	public $renderArray; //Contains all data to be passed to Mustache
 	public $siteDB; //Contains whole DB
+	public $pagetype;
 
 	/* Function to be called on templates render */
 	public function render(){
@@ -16,18 +17,18 @@ class Templates extends Mustache {
 		$themeFolder = ROOT.'themes/'.$this->siteDB['site']['theme'].'/'; //Contains current theme folder path
 		
 		if( !empty( $_GET ) ) {
-			$pagetype = $_GET['pagetype'];
+			$this->pagetype = $_GET['pagetype'];
 		} else {
-			$pagetype = 'index'; //if no page type given - we are on index page
+			$this->pagetype = 'index'; //if no page type given - we are on index page
 		}
 		
 		/* GATHERING GENERAL SITE DATA WITH CAN BE EMBEDED TO ANY PAGE */
 		/* Data from DB */
 		$this->renderArray['homepage'] = $this->siteDB['site']['address']; //Site index page
+		$this->renderArray['theme'] = $this->siteDB['site']['theme']; //Site theme
 		$this->renderArray['title'] = $this->siteDB['site']['title']; //Site title
 		$this->renderArray['subtitle'] = $this->siteDB['site']['subtitle']; //Site subtitle
 		$this->renderArray['artworkscounter'] = $this->siteDB['site']['totalartworks']; //Total artworks count
-		$this->renderArray['mainmenu'] = $this->compileMainMenu(); //Site main menu 
 		$this->renderArray['language'] = $this->siteDB['site']['language']; //Site language
 		$this->renderArray['version'] = $this->siteDB['site']['version']; //Site language
 		
@@ -35,33 +36,36 @@ class Templates extends Mustache {
 		$this->renderArray['totalartworks'] = Utilities::getTranslation( 'totalartworks' ); //Artworks counter translation
 		$this->renderArray['footer'] = Utilities::getTranslation( 'footer' ); //Site footer translation
 		
-		switch ( $pagetype ) {
+		switch ( $this->pagetype ) {
 			case 'article': //Article page. Get article by given pageid
 				if( !$this->complileArticle( $_GET['pageid'] ) ) { //Compilation error
 					$this->complileError();
-					$pagetype = 'error';
+					$this->pagetype = 'error';
 				}
 				break;
 			case 'gallery': //Gallery page. Get gallery by given pageid
 				if( !$this->complileGallery( $_GET['pageid'] ) ) { //Compilation error
 					$this->complileError();
-					$pagetype = 'error';
+					$this->pagetype = 'error';
 				}
 				break;
 			case 'static': //Static page. Get static by given pageid
 				if( !$this->complileStatic( $_GET['pageid'] ) ) { //Compilation error
 					$this->complileError();
-					$pagetype = 'error';
+					$this->pagetype = 'error';
 				}
 				break;
 			case 'tag': //Tag filtered articles. Get all articles by given tag
-				$pagetype = 'index'; //use index page template
+				$this->pagetype = 'index'; //use index page template
 				$this->renderArray['articles_list'] = $this->itterateArticlesByTag( $_GET['pageid'] );
+				$this->renderArray['filtered_tag'] = '"'.$_GET['pageid'].'"';
+				$this->renderArray['filteredtagtitle'] = Utilities::getTranslation( 'filteredtag' );
 				break;
 			case 'index': //Index page. Ganerate articles list
 				$this->renderArray['articles_list'] = $this->itterateArticles( '1' );
 				$totalPages = ceil( count( Articles::returnVisible() ) / $this->siteDB['site']['articlesperpage'] );
 				$this->compilePagination( '1' , $totalPages );
+				$this->renderArray['site_description'] = $this->siteDB['site']['sitedescription']; //Site description
 				break;
 			case 'page': //if user opens next page of blog
 				if( (int)$_GET['pageid'] < 2 ) {
@@ -69,25 +73,28 @@ class Templates extends Mustache {
 				} else {
 					$totalPages = ceil( count( Articles::returnVisible() ) / $this->siteDB['site']['articlesperpage'] );
 					if( $_GET['pageid'] <= $totalPages ) { //if pageid integer and it is less or equal numbers of total pages
-						$pagetype = 'index'; //use index page template
+						$this->pagetype = 'index'; //use index page template
 						$this->compilePagination( $_GET['pageid'] , $totalPages );
 						$this->renderArray['articles_list'] = $this->itterateArticles( $_GET['pageid'] );
+						$this->renderArray['site_description'] = $this->siteDB['site']['sitedescription']; //Site description
 					} else { //return error page
 						$this->complileError();
-						$pagetype = 'error';
+						$this->pagetype = 'error';
 					}
 				}
 				break;
 			default: //If none of this matched - show error page
 				$this->complileError();
-				$pagetype = 'error';
+				$this->pagetype = 'error';
 				break;
 		}
+		
+		$this->renderArray['mainmenu'] = $this->compileMainMenu(); //Site main menu 
 		
 		/* Renders header, body and footer of page */
 		$renderer = new Mustache;
 		echo $renderer->render( file_get_contents( $themeFolder.'header.tpl' ) , $this->renderArray )."\n";
-		echo $renderer->render( file_get_contents( $themeFolder.$pagetype.'.tpl' ) , $this->renderArray )."\n";
+		echo $renderer->render( file_get_contents( $themeFolder.$this->pagetype.'.tpl' ) , $this->renderArray )."\n";
 		echo $renderer->render( file_get_contents( $themeFolder.'footer.tpl' ) , $this->renderArray );
 			
 	}
@@ -105,14 +112,27 @@ class Templates extends Mustache {
 
 	/* Compiles main menu */
 	public function compileMainMenu() {
-		$mainMenu = '<li><a href="'.$this->siteDB['site']['address'].'">'.Utilities::getTranslation( 'blog' ).'</a></li>';
-		$galleries = Galleries::returnVisible();
-		foreach( $galleries as $galleryid => $gallery ) {
-			$mainMenu .= '<li><a href="'.$this->siteDB['site']['address'].'/gallery/'.$galleryid.'">'.$gallery['name'].'</a></li>';
-		}
+		$mainMenu = '';
 		$statics = Statics::returnVisible();
 		foreach( $statics as $staticid => $static ) {
-			$mainMenu .= '<li><a href="'.$this->siteDB['site']['address'].'/static/'.$staticid.'">'.$static['title'].'</a></li>';
+			if( $this->pagetype == 'static' && $staticid == $_GET['pageid'] ) {
+				$mainMenu .= '<li class="current"><a href="'.$this->siteDB['site']['address'].'/static/'.$staticid.'">'.$static['title'].'</a></li>';
+			} else {
+				$mainMenu .= '<li><a href="'.$this->siteDB['site']['address'].'/static/'.$staticid.'">'.$static['title'].'</a></li>';
+			}
+		}
+		$galleries = Galleries::returnVisible();
+		foreach( $galleries as $galleryid => $gallery ) {
+			if( $this->pagetype == 'gallery' && $galleryid == $_GET['pageid'] ) {
+				$mainMenu .= '<li class="current"><a href="'.$this->siteDB['site']['address'].'/gallery/'.$galleryid.'">'.$gallery['name'].'</a></li>';
+			} else {
+				$mainMenu .= '<li><a href="'.$this->siteDB['site']['address'].'/gallery/'.$galleryid.'">'.$gallery['name'].'</a></li>';
+			}
+		}
+		if( $this->pagetype == 'index' || $this->pagetype == 'article' ) {
+			$mainMenu .= '<li class="current"><a href="'.$this->siteDB['site']['address'].'">'.Utilities::getTranslation( 'blog' ).'</a></li>';
+		} else {
+			$mainMenu .= '<li><a href="'.$this->siteDB['site']['address'].'">'.Utilities::getTranslation( 'blog' ).'</a></li>';
 		}
 		return $mainMenu;
 	}
@@ -174,18 +194,18 @@ class Templates extends Mustache {
 			switch ( $totalpages ) {
 				case 2:
 					if( $page == 1 ) {
-						$pagination = '<li class="current">'.$page.'</li><li><a href="'.$this->siteDB['site']['address'].'/page/'.$totalpages.'">'.$totalpages.'</a></li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page+1).'">'.Utilities::getTranslation( 'nextpage' ).'</a> >></li>';
+						$pagination = '<li class="current">'.$page.'</li><li><a href="'.$this->siteDB['site']['address'].'/page/'.$totalpages.'">'.$totalpages.'</a></li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page+1).'">'.Utilities::getTranslation( 'nextpage' ).'</a> &raquo;</li>';
 					} else {
-						$pagination = '<li><< <a href="'.$this->siteDB['site']['address'].'/page/'.($page-1).'">'.Utilities::getTranslation( 'prevpage' ).'</a></li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page-1).'">'.( $page-1 ).'</a></li><li class="current">'.$totalpages.'</li>';
+						$pagination = '<li>&laquo; <a href="'.$this->siteDB['site']['address'].'/page/'.($page-1).'">'.Utilities::getTranslation( 'prevpage' ).'</a></li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page-1).'">'.( $page-1 ).'</a></li><li class="current">'.$totalpages.'</li>';
 					}
 					break;
 				default:
 					if( $page > 1 && $page < $totalpages ) {
-						$pagination = '<li><< <a href="'.$this->siteDB['site']['address'].'/page/'.($page-1).'">'.Utilities::getTranslation( 'prevpage' ).'</a></li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page-1).'">'.( $page-1 ).'</a></li><li class="current">'.$page.'</li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page+1).'">'. ( $page+1 ) .'</a></li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page+1).'">'.Utilities::getTranslation( 'nextpage' ).'</a> >></li>';
+						$pagination = '<li>&laquo; <a href="'.$this->siteDB['site']['address'].'/page/'.($page-1).'">'.Utilities::getTranslation( 'prevpage' ).'</a></li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page-1).'">'.( $page-1 ).'</a></li><li class="current">'.$page.'</li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page+1).'">'. ( $page+1 ) .'</a></li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page+1).'">'.Utilities::getTranslation( 'nextpage' ).'</a> &raquo;</li>';
 					} elseif( $page == 1 ) {
-						$pagination = '<li class="current">'.$page.'</li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page+1).'">'.( $page+1 ).'</a></li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page+2).'">'. ( $page+2 ) .'</a></li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page+1).'">'.Utilities::getTranslation( 'nextpage' ).'</a> >></li>';
+						$pagination = '<li class="current">'.$page.'</li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page+1).'">'.( $page+1 ).'</a></li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page+2).'">'. ( $page+2 ) .'</a></li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page+1).'">'.Utilities::getTranslation( 'nextpage' ).'</a> &raquo;</li>';
 					} elseif( $page == $totalpages )  {
-						$pagination = '<li><< <a href="'.$this->siteDB['site']['address'].'/page/'.($page-1).'">'.Utilities::getTranslation( 'prevpage' ).'</a></li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page-2).'">'.( $page-2 ).'</a></li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page-1).'">'.( $page-1 ).'</a></li><li class="current">'.$page.'</li>';
+						$pagination = '<li>&laquo; <a href="'.$this->siteDB['site']['address'].'/page/'.($page-1).'">'.Utilities::getTranslation( 'prevpage' ).'</a></li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page-2).'">'.( $page-2 ).'</a></li><li><a href="'.$this->siteDB['site']['address'].'/page/'.($page-1).'">'.( $page-1 ).'</a></li><li class="current">'.$page.'</li>';
 					}
 					break;
 			}
