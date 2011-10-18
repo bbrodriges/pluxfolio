6a4afa5039f44eb41cc24e33cbd4aa7682c796aa
bbrodriges <bender@rodriges.org>
2011-10-18 10:25:07 +0400

    Split db

---
+++ core/lib/includer.php
@@ -0,0 +1,11 @@
+<?php 
+
+	include_once('database.class.php'); //must be first
+	include_once('utilities.class.php'); //must be second
+	include_once('galleries.class.php');
+	include_once('statics.class.php');
+	include_once('articles.class.php');
+	include_once('mustache.class.php'); //must be before templates.class.php
+	include_once('templates.class.php');
+
+?>
